65ffdeb607e08c0d1c4b56f463c32cfa67bf695d
Emil Granberg <emil.j.granberg@gmail.com>
2018-06-14 20:02:29 +0200

    Refactor Questomatic.lua
    - Add new functions for accepting and handing in quests.
    - Add isQuestCompleted function.
    - Add debug parameter to the print function and add default parameters.

--- Questomatic.lua
+++ Questomatic.lua
@@ -4,6 +4,8 @@ QUEST_BLACKLIST = {"[19+] Battle of Warsong Gulch",
 					"[?] Past Victories in Warsong Gulch",
 					"[?] Past efforts in Warsong Gulch"}
 
+debug_enabled = false
+
 function Questomatic_OnLoad()
 	this:RegisterEvent("QUEST_PROGRESS");
 	this:RegisterEvent("QUEST_COMPLETE");
@@ -11,58 +13,88 @@ function Questomatic_OnLoad()
 	this:RegisterEvent("QUEST_GREETING");
 	this:RegisterEvent("QUEST_DETAIL");
 
-	print("Questomatic loaded.")
+	print({msg="Questomatic loaded.", debug=true})
 end
 
-function Questomatic_Quest_engine()
+function TurnInActiveGossipQuests()
 	local numActiveGossipQuests = GetNumGossipQuests(GetGossipActiveQuests())
-	print("numActiveGossipQuests "..numActiveGossipQuests)
-	if(numActiveGossipQuests > 0) then
-		for i=1, numActiveGossipQuests, 1 do
-			SelectGossipActiveQuest(i)
-			CompleteQuest()
-			print("GetNumQuestChoices "..GetNumQuestChoices())
-			GetQuestReward(QuestFrameRewardPanel.itemChoice)
-		end
-	else
+	print({msg="numActiveGossipQuests "..numActiveGossipQuests, debug=debug_enabled})
+	for i=1, numActiveGossipQuests, 1 do
+		SelectGossipActiveQuest(i)
 		CompleteQuest()
 		GetQuestReward(QuestFrameRewardPanel.itemChoice)
 	end
+end
 
+function AcceptAvailableGossipQuests()
 	local numAvailableGossipQuests = GetNumGossipQuests(GetGossipAvailableQuests())
-	print("numAvailableGossipQuests "..numAvailableGossipQuests)
-	if(numAvailableGossipQuests > 0) then
-		for i=1, numAvailableGossipQuests, 1 do
-			SelectGossipAvailableQuest(i)
-			AcceptQuest()
-		end
-	else
+	print({msg="numAvailableGossipQuests "..numAvailableGossipQuests, debug=debug_enabled})
+	for i=1, numAvailableGossipQuests, 1 do
+		SelectGossipAvailableQuest(i)
 		AcceptQuest()
-		GetQuestReward()
+	end
+end
+
+function TurnInActiveQuests()
+	local numActiveQuests = GetNumActiveQuests()
+	print({msg="numActiveQuests "..numActiveQuests, debug=debug_enabled})
+	for i=1, numActiveQuests, 1 do
+		is_comlete = isQuestCompleted(GetActiveTitle(i))
+		print({msg="is_comlete "..tostring(is_comlete), debug=debug_enabled})
+		if (is_comlete) then
+			SelectActiveQuest(i)
+		end
+	end
+end
+
+function AcceptAvailableQuests()
+	local numAvailableQuests = GetNumAvailableQuests()
+	print({msg="numAvailableQuests "..numAvailableQuests, debug=debug_enabled})
+	for i=1, numAvailableQuests, 1 do
+		SelectAvailableQuest(i)
 	end
 end
 
 function Questomatic_OnEvent(event, message)
 	if(AreQuestsBlacklisted(GetGossipActiveQuests()) ~= true) then
 		if	(event == "QUEST_PROGRESS") then
-			print("Got QUEST_PROGRESS event")
-
+			print({msg="Got "..event.." event", debug=debug_enabled})
 			CompleteQuest();
 		elseif	(event == "QUEST_COMPLETE") then
-			print("Got QUEST_COMPLETE event")
-			if (GetNumQuestChoices() > 0) then
-				GetQuestReward(QuestFrameRewardPanel.itemChoice);
-			else
-				GetQuestReward();
+			print({msg="Got "..event.." event", debug=debug_enabled})
+			numQuestChoices = GetNumQuestChoices()
+			print({msg="numQuestChoices "..tostring(numQuestChoices), debug=debug_enabled})
+			if (numQuestChoices == 0) then
+				GetQuestReward()
 			end
-		elseif	(event == "GOSSIP_SHOW" or "QUEST_DETAIL") then
-			print("Got "..event.." event")
-			Questomatic_Quest_engine()
-		elseif	(event == "QUEST_GREETING") then
-			print("Got QUEST_GREETING event")
+		elseif	(event == "GOSSIP_SHOW") then
+			print({msg="Got "..event.." event", debug=debug_enabled})
+			TurnInActiveGossipQuests()
+			AcceptAvailableGossipQuests()
 
+		elseif	(event == "QUEST_DETAIL") then
+			print({msg="Got "..event.." event", debug=debug_enabled})
+			AcceptQuest()
+
+		elseif	(event == "QUEST_GREETING") then
+			print({msg="Got "..event.." event", debug=debug_enabled})
+			TurnInActiveQuests(numActiveQuests)
+			AcceptAvailableQuests(numAvailableQuests)
 		end
 	end
+end
+
+function isQuestCompleted(questname)
+	local i = 1
+	while GetQuestLogTitle(i) do
+		local questTitle, level, questTag, isHeader, isCollapsed, isComplete, isDaily = GetQuestLogTitle(i)
+		if(questTitle == questname) then
+			return isComplete
+		end
+		i = i + 1
+	end
+	print({msg="Quest "..questname.." not tracked.", debug=debug_enabled})
+	return
 end
 
 function GetNumGossipQuests(...)
@@ -83,12 +115,15 @@ function PrintReturns(...)
 	if(arg.n == 0) then
 		return
 	end
-	print("length "..arg.n)
+	print({msg="length "..arg.n, debug=debug_enabled})
 	for i=1, arg.n, 1 do
-		print("arg"..i.." "..arg[i])
+		print({msg="arg"..i.." "..tostring(arg[i]), debug=debug_enabled})
 	end
 end
 
-function print(msg)
-	DEFAULT_CHAT_FRAME:AddMessage(msg)
+function print(t)
+	setmetatable(t,{__index={msg="test ",debug=false}})
+	if t.debug then
+		DEFAULT_CHAT_FRAME:AddMessage(t.msg)
+	end
 end
