cefb1d53316041007460688febdf242d323592a6
Emil Granberg <emil.j.granberg@gmail.com>
2018-06-14 02:44:35 +0200

    Add blacklist for unusual quests

--- Questomatic.lua
+++ Questomatic.lua
@@ -1,4 +1,8 @@
 
+QUEST_BLACKLIST = {"[19+] Battle of Warsong Gulch",
+					"[?] Past Victories in Arathi",
+					"[?] Past Victories in Warsong Gulch",
+					"[?] Past efforts in Warsong Gulch"}
 
 function Questomatic_OnLoad()
 	this:RegisterEvent("QUEST_PROGRESS");
@@ -11,7 +15,6 @@ function Questomatic_OnLoad()
 end
 
 function Questomatic_Quest_engine()
-
 	local numActiveGossipQuests = GetNumGossipQuests(GetGossipActiveQuests())
 	print("numActiveGossipQuests "..numActiveGossipQuests)
 	if(numActiveGossipQuests > 0) then
@@ -37,24 +40,28 @@ function Questomatic_Quest_engine()
 		AcceptQuest()
 		GetQuestReward()
 	end
-
 end
 
 function Questomatic_OnEvent(event, message)
-	if	(event == "QUEST_PROGRESS") then
-		print("Got QUEST_PROGRESS event")
-		CompleteQuest();
-	elseif	(event == "QUEST_COMPLETE") then
-		print("Got QUEST_COMPLETE event")
-		if (GetNumQuestChoices() == 0) then
-			GetQuestReward(QuestFrameRewardPanel.itemChoice);
-		end
-	elseif	(event == "GOSSIP_SHOW" or "QUEST_DETAIL") then
-		print("Got "..event.." event")
-		Questomatic_Quest_engine()
-	elseif	(event == "QUEST_GREETING") then
-		print("Got QUEST_GREETING event")
+	if(AreQuestsBlacklisted(GetGossipActiveQuests()) ~= true) then
+		if	(event == "QUEST_PROGRESS") then
+			print("Got QUEST_PROGRESS event")
 
+			CompleteQuest();
+		elseif	(event == "QUEST_COMPLETE") then
+			print("Got QUEST_COMPLETE event")
+			if (GetNumQuestChoices() > 0) then
+				GetQuestReward(QuestFrameRewardPanel.itemChoice);
+			else
+				GetQuestReward();
+			end
+		elseif	(event == "GOSSIP_SHOW" or "QUEST_DETAIL") then
+			print("Got "..event.." event")
+			Questomatic_Quest_engine()
+		elseif	(event == "QUEST_GREETING") then
+			print("Got QUEST_GREETING event")
+
+		end
 	end
 end
 
@@ -62,13 +69,13 @@ function GetNumGossipQuests(...)
 	return arg.n/2
 end
 
-function GetQuests(...)
-	if(arg.n == 0) then
-		return
-	end
-	for i=1, arg.n-1, 2 do
-		print("quest "..arg[i])
-		print("level "..arg[i+1])
+function AreQuestsBlacklisted(...)
+	for i=1, arg.n, 2 do
+		for j=1, getn(QUEST_BLACKLIST), 1 do
+			if(QUEST_BLACKLIST[j] == arg[i]) then
+				return true
+			end
+		end
 	end
 end
 
