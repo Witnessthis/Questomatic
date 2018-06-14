e0c8c66b43d868ab841557f84baf7d302806469f
Emil Granberg <emil.j.granberg@gmail.com>
2018-06-14 20:02:29 +0200

    Further Refactor Questomatic
    - Add QM slash command with debug print option
    - Add LUA event handler and remove XML entry point

--- Questomatic.lua
+++ Questomatic.lua
@@ -1,19 +1,47 @@
 
-QUEST_BLACKLIST = {"[19+] Battle of Warsong Gulch",
-					"[?] Past Victories in Arathi",
-					"[?] Past Victories in Warsong Gulch",
-					"[?] Past efforts in Warsong Gulch"}
+function isQuestCompleted(questname)
+	local i = 1
+	while GetQuestLogTitle(i) do
+		local questTitle, level, questTag, isHeader, isCollapsed, isComplete, isDaily = GetQuestLogTitle(i)
+		if(questTitle == questname) then
+			return isComplete
+		end
+		i = i + 1
+	end
+	print({msg="Quest "..questname.." not tracked.", debug=debug_enabled})
+	return
+end
 
-debug_enabled = false
+function GetNumGossipQuests(...)
+	return arg.n/2
+end
 
-function Questomatic_OnLoad()
-	this:RegisterEvent("QUEST_PROGRESS");
-	this:RegisterEvent("QUEST_COMPLETE");
-	this:RegisterEvent("GOSSIP_SHOW");
-	this:RegisterEvent("QUEST_GREETING");
-	this:RegisterEvent("QUEST_DETAIL");
+function AreQuestsBlacklisted(...)
+	for i=1, arg.n, 2 do
+		for j=1, getn(QUEST_BLACKLIST), 1 do
+			if(QUEST_BLACKLIST[j] == arg[i]) then
+				return true
+			end
+		end
+	end
+end
 
-	print({msg="Questomatic loaded.", debug=true})
+-- Mainly used for debugging purposes
+function PrintReturns(...)
+	if(arg.n == 0) then
+		return
+	end
+	print({msg="length "..arg.n, debug=debug_enabled})
+	for i=1, arg.n, 1 do
+		print({msg="arg"..i.." "..tostring(arg[i]), debug=debug_enabled})
+	end
+end
+
+function print(t)
+	setmetatable(t,{__index={msg="test ",debug=false}})
+	if t.debug then
+		DEFAULT_CHAT_FRAME:AddMessage(t.msg)
+	end
 end
 
 function TurnInActiveGossipQuests()
@@ -55,11 +83,41 @@ function AcceptAvailableQuests()
 	end
 end
 
-function Questomatic_OnEvent(event, message)
+QUEST_BLACKLIST = {"[19+] Battle of Warsong Gulch",
+					"[?] Past Victories in Arathi",
+					"[?] Past Victories in Warsong Gulch",
+					"[?] Past efforts in Warsong Gulch"}
+
+debug_enabled = false
+
+SLASH_QM1 = "/qm"
+SLASH_QM2 = "/questomatic"
+SlashCmdList["QM"] = function(msg)
+	if (msg == "-d") then
+		debug_enabled = not debug_enabled
+	else
+		print({msg="/qm [-d]", debug=true})
+		print({msg="-d Turn debug printing on/off", debug=true})
+	end
+end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_LOGIN")
+frame:RegisterEvent("QUEST_PROGRESS")
+frame:RegisterEvent("QUEST_COMPLETE")
+frame:RegisterEvent("GOSSIP_SHOW")
+frame:RegisterEvent("QUEST_GREETING")
+frame:RegisterEvent("QUEST_DETAIL")
+
+local function eventHandler(...)
 	if(AreQuestsBlacklisted(GetGossipActiveQuests()) ~= true) then
-		if	(event == "QUEST_PROGRESS") then
+		if (event == "PLAYER_LOGIN") then
+			print({msg="Questomatic loaded. /qm", debug=true})
+
+		elseif	(event == "QUEST_PROGRESS") then
 			print({msg="Got "..event.." event", debug=debug_enabled})
 			CompleteQuest();
+
 		elseif	(event == "QUEST_COMPLETE") then
 			print({msg="Got "..event.." event", debug=debug_enabled})
 			numQuestChoices = GetNumQuestChoices()
@@ -67,6 +125,7 @@ function Questomatic_OnEvent(event, message)
 			if (numQuestChoices == 0) then
 				GetQuestReward()
 			end
+
 		elseif	(event == "GOSSIP_SHOW") then
 			print({msg="Got "..event.." event", debug=debug_enabled})
 			TurnInActiveGossipQuests()
@@ -84,46 +143,4 @@ function Questomatic_OnEvent(event, message)
 	end
 end
 
-function isQuestCompleted(questname)
-	local i = 1
-	while GetQuestLogTitle(i) do
-		local questTitle, level, questTag, isHeader, isCollapsed, isComplete, isDaily = GetQuestLogTitle(i)
-		if(questTitle == questname) then
-			return isComplete
-		end
-		i = i + 1
-	end
-	print({msg="Quest "..questname.." not tracked.", debug=debug_enabled})
-	return
-end
-
-function GetNumGossipQuests(...)
-	return arg.n/2
-end
-
-function AreQuestsBlacklisted(...)
-	for i=1, arg.n, 2 do
-		for j=1, getn(QUEST_BLACKLIST), 1 do
-			if(QUEST_BLACKLIST[j] == arg[i]) then
-				return true
-			end
-		end
-	end
-end
-
-function PrintReturns(...)
-	if(arg.n == 0) then
-		return
-	end
-	print({msg="length "..arg.n, debug=debug_enabled})
-	for i=1, arg.n, 1 do
-		print({msg="arg"..i.." "..tostring(arg[i]), debug=debug_enabled})
-	end
-end
-
-function print(t)
-	setmetatable(t,{__index={msg="test ",debug=false}})
-	if t.debug then
-		DEFAULT_CHAT_FRAME:AddMessage(t.msg)
-	end
-end
+frame:SetScript("OnEvent", eventHandler)
